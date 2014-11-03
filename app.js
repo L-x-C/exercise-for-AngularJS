9c4b3f5c841c61a3e888eb0bc6e3b2c82412e219
L_x_C <lxc.neal@gmail.com>
2014-11-03 20:07:36 +0800

    true scope

--- app.js
+++ app.js
@@ -4,24 +4,42 @@
 * Description
 */
 var app = angular.module('myApp', []);
-app.controller('MainController', function($scope){
+// app.controller('MainController', function($scope){
 
-}).directive('myDirective', ['', function(){
+// })
+app.directive('myDirective', function(){
 	// Runs during compile
 	return {
 		// name: '',
-		priority: 100,
+		// priority: 100,
 		// terminal: true,
 		scope: {}, // {} = isolate, true = child, false/undefined = no change
 		// controller: function($scope, $element, $attrs, $transclude) {},
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-		template: '<div>Inside mymymy {{myProperty=}}</div>'
+		template: 'Inside myDirective {{myProperty}}'
 		// templateUrl: '',
 		// replace: true,
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 
 	};
-}]);
+}).directive('myInheritScopeDirective', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		scope: true, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		template: 'Inside Inherit {{myProperty}}'
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+
+	};
+});
 
