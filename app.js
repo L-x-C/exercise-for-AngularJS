90d6e396e136c2fd8ae253806e56b2d2d33b0a1b
L_x_C <lxc.neal@gmail.com>
2014-11-03 19:58:23 +0800

    isolate scope

--- app.js
+++ app.js
@@ -4,9 +4,24 @@
 * Description
 */
 var app = angular.module('myApp', []);
-app.controller('myCon', function($scope){
-	$scope.generateNum = function() {
-		return Math.floor((Math.random()*10)+1);
-	}	
-})
+app.controller('MainController', function($scope){
+
+}).directive('myDirective', ['', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		priority: 100,
+		// terminal: true,
+		scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		template: '<div>Inside mymymy {{myProperty=}}</div>'
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+
+	};
+}]);
 
